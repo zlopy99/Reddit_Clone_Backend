0f07ecd1f11ae504c1bb79c02562d50322c08d59
Vinko-Tino <Zlopasa@hotmail.com>
2023-04-23 22:43:55 +0200

    Configuring CORSE

--- src/main/java/com/app/reddit/RedditApplication.java
+++ src/main/java/com/app/reddit/RedditApplication.java
@@ -1,6 +1,5 @@
 package com.app.reddit;
 
-import com.app.reddit.config.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
